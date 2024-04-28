569a94992695ff73fb2aa5c9ff88689bec688e0e
EunKyeong99 <qqqaak@naver.com>
2024-04-28 19:52:09 +0900

    mid term project - vintage filter - analog filter - dreamy filter - face warping filter - custom filter

--- mid_term_project/face_warping_filter.py
+++ mid_term_project/face_warping_filter.py
@@ -1,13 +1,5 @@
-import cv2
+import numpy as np, cv2
 import dlib
-import numpy as np
-
-def highlight_features(image, landmarks):
-    output = image.copy()
-    # 각 랜드마크에 대해 원을 그려 강조
-    for (x, y) in landmarks:
-        cv2.circle(output, (x, y), 2, (0, 255, 0), -1)  # 녹색 원으로 표시
-    return output
 
 def load_landmarks(image, predictor, detector):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -17,29 +9,38 @@ def load_landmarks(image, predictor, detector):
         return np.array([[p.x, p.y] for p in landmarks.parts()], dtype=np.int32)
     return None
 
-# 이미지 및 모델 로드
-image_path = './image/Lenna.png'
-model_path = './model/shape_predictor_68_face_landmarks.dat'
-image = cv2.imread(image_path)
-if image is None:
-    raise Exception("영상 파일 읽기 오류")
+
+def emphasize_eyes(image, landmarks, scale=2):
+    if landmarks is None:
+        return image
+
+    output_image = image.copy()
+    for i in range(36, 48):
+        if i == 42:
+            x, y, w, h = cv2.boundingRect(landmarks[36:42])
+        else:
+            x, y, w, h = cv2.boundingRect(landmarks[42:48])
+
+        roi = image[y:y + h, x:x + w]
+        roi = cv2.resize(roi, (w * scale, h * scale), interpolation=cv2.INTER_CUBIC)
+
+        center_x, center_y = x + w // 2, y + h // 2
+        new_x = max(0, center_x - w * scale // 2)
+        new_y = max(0, center_y - h * scale // 2)
+
+        output_image[new_y:new_y + h * scale, new_x:new_x + w * scale] = roi
+
+    return output_image
+
+image = cv2.imread('Lenna.png')
+if image is None: raise Exception("영상 파일 읽기 오류")
 
 detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor(model_path)
-
-# 랜드마크 탐지
+predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
 landmarks = load_landmarks(image, predictor, detector)
 
-if landmarks is not None:
-    # 특징점 강조
-    highlighted_image = highlight_features(image, landmarks)
+emphasized_image = emphasize_eyes(image, landmarks)
 
-    # 결과 표시
-    cv2.imshow('Original Image', image)
-    cv2.imshow('Highlighted Features', highlighted_image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-else:
-    print("No faces detected.")
-
-
+cv2.imshow('original', image)
+cv2.imshow('Emphasized Eyes', emphasized_image)
+cv2.waitKey(0)
