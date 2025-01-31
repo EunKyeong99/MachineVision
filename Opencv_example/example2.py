d6a1f4c88ee0de5d010102e3c7befb5befdd199b
EunKyeong <163232784+EunKyeong99@users.noreply.github.com>
2025-01-31 11:11:39 +0900

    Update example2.py

--- Opencv_example/example2.py
+++ Opencv_example/example2.py
@@ -1,8 +1,10 @@
 import numpy as np
 import cv2
+// cv2는 이미지 처리와 관련된 함수를 제공하는 라이브러리
 
 image = np.zeros((200, 300), np.uint8)
 image.fill(255)
+// 픽셀값 255는 흰색
 
 title1, title2 = 'AUTOSIZE', 'NORMAL'
 cv2.namedWindow(title1, cv2.WINDOW_AUTOSIZE)
